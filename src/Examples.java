b0ad48dd30bd0724a27b58ff48a42502e4582949
Eddie Stump <ejstump13@gmail.com>
2022-04-04 22:51:25 -0400

    Finished 13 - 14

--- src/Examples.java
+++ src/Examples.java
@@ -9,9 +9,9 @@ public class Examples {
     LinkedList<ShootingRound> rounds1(){
         LinkedList<ShootingRound> rounds = new LinkedList<>();
         rounds.add(new ShootingRound(5, true));
-        rounds.add(new ShootingRound(2, false));
-        rounds.add(new ShootingRound(1, true));
-        rounds.add(new ShootingRound(0, false));
+        rounds.add(new ShootingRound(2, true));
+        rounds.add(new ShootingRound(5, true));
+        rounds.add(new ShootingRound(0, true));
         return rounds;
     }
 
@@ -41,6 +41,57 @@ public class Examples {
     Athlete jerry = new Athlete(fResult1, "Jerry");
     Athlete larry = new Athlete(fResult2, "Larry");
 
+    public LinkedList<Athlete> getOlympicsAthletes() {
+        LinkedList<Athlete> temp = new LinkedList<>();
+        temp.add(jerry);
+        temp.add(larry);
+
+        return temp;
+    }
+
+    public LinkedList<Athlete> getComp2Athletes() {
+        LinkedList<Athlete> temp = new LinkedList<>();
+        LinkedList<ShootingRound> testRounds = new LinkedList<>();
+        testRounds.add(new ShootingRound(5, false));
+        temp.add(new Athlete(new FinalResult(new ShootingResult(testRounds),
+                new SkiingResult(0, 0, 0, 0, 0, 0)), "test1"));
+        temp.add(jerry);
+        temp.add(larry);
+        temp.add(new Athlete(new FinalResult(new ShootingResult(new LinkedList<ShootingRound>()),
+                new SkiingResult(0, 0, 0, 0, 0, 0)), "test2"));
+
+        return temp;
+    }
+
+
+    // Competitions
+    Competition emptyComp = new Competition(5, new LinkedList<Athlete>());
+
+    Competition olympics = new Competition(4, getOlympicsAthletes());
+    Competition olympicsWithMoreRounds = new Competition(5, getOlympicsAthletes());
+
+    Competition comp2_1 = new Competition(1, getComp2Athletes());
+    Competition comp2_2 = new Competition(3, getComp2Athletes());
+
+    // Other competitions used for testing anyImprovement
+    public LinkedList<Athlete> getAnyImprovementAthletes_Better() {
+        LinkedList<Athlete> temp = new LinkedList<>();
+        temp.add(new Athlete(new FinalResult(new ShootingResult(new LinkedList<ShootingRound>()),
+                new SkiingResult(0, 1, 0, 0, 0, 0)), "test1"));
+
+        return temp;
+    }
+
+    public LinkedList<Athlete> getAnyImprovementAthletes_Worse() {
+        LinkedList<Athlete> temp = new LinkedList<>();
+        temp.add(new Athlete(new FinalResult(new ShootingResult(new LinkedList<ShootingRound>()),
+                new SkiingResult(0, -1, 0, 0, 0, 0)), "test1"));
+
+        return temp;
+    }
+
+    Competition test_better = new Competition(4, getAnyImprovementAthletes_Better());
+    Competition test_worse = new Competition(4, getAnyImprovementAthletes_Worse());
 
     @Test
     public void testPointsEarned_Skiing() {
@@ -54,7 +105,7 @@ public class Examples {
 
     @Test
     public void testPointsEarned_Shooting() {
-        assertEquals(shootingResult1.pointsEarned(), 8, 0.01);
+        assertEquals(shootingResult1.pointsEarned(), 12, 0.01);
     }
 
     @Test
@@ -69,12 +120,12 @@ public class Examples {
 
     @Test
     public void testPenalties_Shooting() {
-        assertEquals(shootingResult1.getPenalties(), 720, 0.01);
+        assertEquals(shootingResult1.getPenalties(), 480, 0.01);
     }
 
     @Test
     public void testFinalScore_jerry() {
-        assertEquals(fResult1.finalScore(), 886.6, 0.01);
+        assertEquals(fResult1.finalScore(), 646.6, 0.01);
     }
 
     @Test
@@ -126,6 +177,94 @@ public class Examples {
     @Test
     public void testPointsEarned_MassStart2() {
         assertEquals(massStartResult2.pointsEarned(), 80, 0.01);
+    }
+
+    @Test
+    public void testBestRoundByType_noRounds() {
+        assertEquals(shootingResult1.bestRoundByType(false), null);
+    }
+
+    @Test
+    public void testBestRoundsByType_tie() {
+        assertEquals(shootingResult1.bestRoundByType(true), new ShootingRound(5, true));
+    }
+
+    @Test
+    public void testBestRoundsByType_someOfEach() {
+        assertEquals(shootingResult2.bestRoundByType(true), new ShootingRound(5, true));
+    }
+
+    @Test
+    public void testBestRoundsByType_someOfEach2() {
+        assertEquals(shootingResult2.bestRoundByType(false), new ShootingRound(4, false));
+    }
+
+    @Test
+    public void testShootingDNF_empty() {
+        assertEquals(emptyComp.shootingDNF(), new LinkedList<Athlete>());
+    }
+
+    @Test
+    public void testShootingDNF_allFinished() {
+        assertEquals(olympics.shootingDNF(), new LinkedList<Athlete>());
+    }
+
+    @Test
+    public void testShootingDNF_noneFinished() {
+        LinkedList<String> temp = new LinkedList<>();
+        temp.add("Jerry");
+        temp.add("Larry");
+        assertEquals(olympicsWithMoreRounds.shootingDNF(), temp);
+    }
+
+    @Test
+    public void testShootingDNF_someFinished1() {
+        LinkedList<String> temp = new LinkedList<>();
+        temp.add("test2");
+        assertEquals(comp2_1.shootingDNF(), temp);
+    }
+
+    @Test
+    public void testShootingDNF_someFinished2() {
+        LinkedList<String> temp = new LinkedList<>();
+        temp.add("test1");
+        temp.add("test2");
+        assertEquals(comp2_2.shootingDNF(), temp);
+    }
+
+    @Test
+    public void testFinalScoreForAthlete_first() {
+        assertEquals(olympics.finalScoreForAthlete("Jerry"), 646.6, 0.01);
+    }
+
+    @Test
+    public void testFinalScoreForAthlete_last() {
+        assertEquals(olympics.finalScoreForAthlete("Larry"), 544.5, 0.01);
+    }
+
+    @Test
+    public void testFinalScoreForAthlete_zeroScore(){
+        assertEquals(comp2_1.finalScoreForAthlete("test1"), 0, 0.01);
+    }
+
+    @Test
+    public void testFinalScoreForAthlete_otherLocationInList(){
+        assertEquals(comp2_1.finalScoreForAthlete("Jerry"), 646.6, 0.01);
+    }
+
+    @Test
+    public void testAnyImprovement_better() {
+        assertTrue(test_better.anyImprovement(comp2_1));
+    }
+
+    @Test
+    public void testAnyImprovement_same() {
+        assertFalse(olympics.anyImprovement(olympics));
+    }
+
+    @Test
+    public void testAnyImprovement_worse() {
+        assertFalse(test_worse.anyImprovement(comp2_1));
     }
 
 
